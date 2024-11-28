d7a1a572fbd536c260a97d7d20d9d596ed58d953
doodiyehezkel <doodiyehezkel@gmail.com>
2024-11-28 23:41:15 +0200

    change router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import HomePage from './pages/home/HomePage.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} > 
         <Route index element={<HomePage />}/>
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
