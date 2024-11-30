be05f7595545b35749cd6e1f91cd2f37119fe66c
doodiyehezkel <doodiyehezkel@gmail.com>
2024-11-30 22:11:00 +0200

    add support for mobile

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,23 +2,25 @@ import Navigation from "../navigation/Navigation";
 import style from './Header.module.css'
 import smartPhoneSvg from '../../assets/svg/smartphone.svg'
 import whatappImg from '../../assets/images/whatsapp_logo_green.png'
+
 export default function Header() {
+
+
+
+
     return (
         <header className={style['header']}>
-            <div>
-
-            </div>
-            <div>
+            <div className="header-nav">
                 <Navigation />
             </div>
             <div className={style['call-me']}>
-            <a className={style['phone']} href="tel:+972 50-677-1525">
+                <a className={style['phone']} href="tel:+972 50-677-1525">
                     050-677-1525
                     <img src={smartPhoneSvg} alt="" />
                 </a>
                 <a className={style['whatapp']} href="https://wa.me/972506771525?text=היי%20הגעתי%20דרך%20האתר%20אני%20מעוניין%20בפרטים%20נוספים%20">
                     <img src={whatappImg} alt="" />
-                </a>    
+                </a>
             </div>
         </header>
     )
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,11 +1,34 @@
 import { NavLink } from "react-router";
 import style from './Navigation.module.css'
+import useWindowSize from "../../hook/useWindowSize";
+import { useState } from "react";
+import hamburgerMenuSvg from '../../assets/svg/hamburger-menu.svg'
+
+
 export default function Navigation() {
+
+    const [isMobileManuOpen, setIsMobileManuOpen] = useState(false)
+
+    const [width] = useWindowSize()
+
     return (
-        <nav className={style['nav-container']}>
-            <NavLink to={"/"}>דף הבית</NavLink>
-            <NavLink to={"/"}>רשת ביתית וסיבים אופטים</NavLink>
-            <NavLink to={"/"}>אינטרקום</NavLink>
-        </nav>
+        width >= 700 ?
+            <nav className={style['nav-container']}>
+                <NavLink to={"/"}>דף הבית</NavLink>
+                <NavLink to={"/"}>רשת ביתית וסיבים אופטים</NavLink>
+                <NavLink to={"/"}>אינטרקום</NavLink>
+            </nav>
+            :
+            <div>
+                 <img onClick={() => setIsMobileManuOpen(prev => { return !prev })} className={style['mobile-nav-container-manu']} src={hamburgerMenuSvg} alt="hamburgerMenuSvg" />
+                {
+                    isMobileManuOpen &&
+                    <nav className={style['mobile-nav-container']} >
+                        <NavLink to={"/"}>דף הבית</NavLink>
+                        <NavLink to={"/"}>רשת ביתית וסיבים אופטים</NavLink>
+                        <NavLink to={"/"}>אינטרקום</NavLink>
+                    </nav>
+                }
+            </div>
     )
 }
